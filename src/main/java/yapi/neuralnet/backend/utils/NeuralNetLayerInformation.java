5de67f200088e7027bdde51e8c39282b935156f0
jojo <yoyosource@nidido.de>
2020-02-07 23:27:13 +0100

    Add size check

--- src/main/java/yapi/neuralnet/backend/utils/NeuralNetLayerInformation.java
+++ src/main/java/yapi/neuralnet/backend/utils/NeuralNetLayerInformation.java
@@ -5,6 +5,9 @@ public class NeuralNetLayerInformation {
     private int size = -1;
 
     NeuralNetLayerInformation(int size) {
+        if (size < 1) {
+            throw new IllegalArgumentException("size needs to be at least 1");
+        }
         this.size = size;
     }
 
